000a2ea06584229ca6f225b9a21899ab64e0f7b3
vieirahadassa-flash <vieira.hadassa@escola.pr.gov.br>
2026-08-19 11:07:38 -0300

    s

---
+++ script.js
@@ -0,0 +1,53 @@
+// Selecione todos os elementos <button> da página e os guarda na constante 'botoes' como lista 
+const botoes = document.querySelectorAll("button")
+
+// Passa por cada botão lista (um por um) para applicar as regras abaixo 
+botoes.forEach(function (botao) {
+
+// Cria uma variável interma para controlar se O BOTÃO ATUAL foi clicado ou não (começa como falso)
+let curtiu = false;
+
+// Adiciona um "escutador" que aguarda especificamente neste botão 
+botao.addEventListener("click, botaoClicado")
+
+// Define a função que roda toda vez que esse botão específico recebe um clique 
+function botaoClicado(){
+    
+}
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
